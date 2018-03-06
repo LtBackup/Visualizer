d57b90608b1649d116aeb839f594b84cf4f031ec
cybernetck <mattswomack@gmail.com>
2018-03-06 16:36:25 -0600

    Created HTML Route

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+
+var path = require("path");
+
+module.exports = function(app) {
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+}
