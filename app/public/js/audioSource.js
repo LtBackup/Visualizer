0b6fcc878eb47bb11c08810f4db3a738f0fdaa27
LtBackup <jamespratt6@gmail.com>
2018-03-19 22:28:27 -0500

    cleans up various files and deletes extraneous files

--- app/public/js/audioSource.js
+++ app/public/js/audioSource.js
@@ -4,7 +4,6 @@ var audio = false;
 
 window.onload = function () {
 
-   console.log('audio loader connected');
    window.addEventListener('drop', onDrop, false);
    window.addEventListener('dragover', onDrag, false);
 
@@ -18,37 +17,26 @@ window.onload = function () {
        e.preventDefault();
        var droppedFiles = e.dataTransfer.files;
       $('#instructions').fadeOut(1000);
-       initiateAudio(droppedFiles[0]); // initiates audio from the dropped file
+       initiateAudio(droppedFiles[0]);
    }
    function animationFrame(){
-       console.log("Hi. Animation canceled.")
    };
+
    function initiateAudio(data) {
-    //    if (animationFrame) {
-    //     window.cancelAnimationFrame(animationFrame);
-    //    }
        if (audio) {
            audio.pause();
            window.cancelAnimationFrame(animationFrame);
-          // window.onanimationcancel = window.onload;
        }
        audio = document.createElement('audio'); // creates an html audio element
        audio.src = URL.createObjectURL(data); // sets the audio source to the dropped file
        audio.autoplay = true;
-       // app.audio.play();
        var play = true;
        document.body.appendChild(audio);
 
        if (typeof context !== 'undefined'){
-        console.log("Out with the old.. in with the new!");
         context.close();
         }
-       // app.ctx = new (window.AudioContext || window.webkitAudioContext)(); // creates audioNode
-       // source = app.ctx.createMediaElementSource(app.audio); // creates audio source
-       // analyser = app.ctx.createAnalyser(); // creates analyserNode
-       // source.connect(app.ctx.destination); // connects the audioNode to the audioDestinationNode (computer speakers)
-       // source.connect(analyser); // connects the analyser node to the audioNode and the audioDestinationNode
-       console.log("sending MP3 dropped on page", audio.src);
+       
        audioAnalyzer(audio);
    }
 
