d541b8272f53098592bfc0101d81fdce6e688e06
LtBackup <jamespratt6@gmail.com>
2018-03-20 20:00:19 -0500

    proper orbiting established

--- app/public/js/scene.js
+++ app/public/js/scene.js
@@ -36,7 +36,7 @@ renderer.setClearColor(0x333F47);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 3000);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 4000);
 
 var scene = new THREE.Scene();
 
@@ -115,14 +115,38 @@ var sphereMesh4 = new THREE.Mesh(sphere4, sphereMaterial4);
 var sphereMesh5 = new THREE.Mesh(sphere5, sphereMaterial5);
 var spherePlane= new THREE.PlaneBufferGeometry(8000, 8000);
 var _material= new THREE.MeshToonMaterial({ color: 0xFFFFFF })
+var point = new THREE.Object3D();
+//scene.add(sphereMesh1, sphereMesh2, sphereMesh3, sphereMesh4, sphereMesh5);
+scene.add(sphereMesh3, point);
 
 meshArr3.push(sphereMesh1, sphereMesh2, sphereMesh3, sphereMesh4, sphereMesh5);
 
+point.position.set(0,0,-3000);
+
+//new
+var pivot1 = new THREE.Object3D();
+// sun.add(pivot1);
+point.add(pivot1);
+var pivot2 = new THREE.Object3D();
+point.add(pivot2);
+var pivot4 = new THREE.Object3D();
+point.add(pivot4);
+var pivot5 = new THREE.Object3D();
+point.add(pivot5);
+
+//scene.add(pivot1, pivot2, pivot4, pivot5);
 sphereMesh1.position.set(-1100, 0, -3000);
 sphereMesh2.position.set(-500, 0, -3000);
 sphereMesh3.position.set(0, 0, -3000);
 sphereMesh4.position.set(500, 0, -3000);
 sphereMesh5.position.set(1000, 0, -3000);
+
+pivot1.add(sphereMesh1);
+pivot2.add(sphereMesh2);
+pivot4.add(sphereMesh4);
+pivot5.add(sphereMesh5);
+
+
 
 var groundMaterial = new THREE.MeshPhongMaterial({
     shininess: 80,
@@ -135,7 +159,7 @@ ground.rotation.x = -Math.PI / 2;
 ground.position.set( 0, 0, 0 );
 ground.scale.set( 1000, 1000, 1000 );
 ground.receiveShadow = true;
-scene.add(sphereMesh1, sphereMesh2, sphereMesh3, sphereMesh4, sphereMesh5, ground);
+scene.add(ground);
 
 //Scene 4 visual elements
 var geometry1 = new THREE.BoxGeometry(100, 10, 200);
@@ -314,7 +338,7 @@ function render(ts) {
             var scalar = averages[0] / 50
 
             for (var i = 0; i < meshArr2.length; i++) {
-                meshArr2[i].rotation.x += 0.01;
+                meshArr2[i].rotation.y += 0.01;
                 meshArr2[i].rotation.y += 0.01;
 
                 meshArr2[i].scale.y = scalar; // SCALE
@@ -324,13 +348,19 @@ function render(ts) {
             break;
 
         case 3:
-            if (i === sphere1.vertices.length) {
-                i = 0;
-            }
-            sphereMesh1.rotation.x += 0.02;
-            sphereMesh1.rotation.y += 0.02;
-            sphereMesh2.rotation.z += 0.02;
-            sphereMesh2.rotation.y += 0.02;
+            // if (i === sphere1.vertices.length) {
+            //     i = 0;
+            // }
+            // sphereMesh1.rotation.x += 0.02;
+            // sphereMesh1.rotation.y += 0.02;
+            // sphereMesh2.rotation.z += 0.02;
+            // sphereMesh2.rotation.y += 0.02;
+
+            pivot1.rotation.z += .01;
+            pivot2.rotation.z -= .01;
+            pivot4.rotation.z += .01;
+            pivot5.rotation.z -= .01;
+
 
             var scalars = [];
             for (let j = 0; j < 5; j++) {
